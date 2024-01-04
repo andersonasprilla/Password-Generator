277babaff09fbe2fe843b588c5dfe427509db384
Anderson Asprilla <anderson.asprilla@gmail.com>
2024-01-03 19:31:12 -0500

    initial commit

---
+++ Develop/script.js
@@ -0,0 +1,30 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+}
+
+// Generate password function
+function generatePassword() {
+  // Define character sets
+  var lowercaseChars = "abcdefghijklmnopqrstuvwxyz";
+  var uppercaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var numericChars = "0123456789";
+  var specialChars = "!@#$%^&*()_-+=<>?";
+
+}
+
+
+
+
+
+
+
